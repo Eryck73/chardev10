b90a2ee37586dbdbbedc4660810d82edcfdb1fda
chardev <lemartin@chardev.org>
2012-07-08 17:31:00 +0200

    Changed from IIFE to std. class definition

--- website/public/js/engine/entities/User.js
+++ website/public/js/engine/entities/User.js
@@ -1,22 +1,17 @@
-var User;
+/**
+ * @constructor
+ * @param {Array} serialized
+ */
+var User = function( serialized ) {
+	this.id = serialized[0];
+	this.name = serialized[1];
+	this.region = serialized[2];
+	this.battleNetProfiles = serialized[3];
+};
 
-(function(){
-	/**
-	 * 
-	 * @constructor
-	 * @param {array} serialized
-	 */
-	User = function( serialized ) {
-		this.id = serialized[0];
-		this.name = serialized[1];
-		this.region = serialized[2];
-		this.battleNetProfiles = serialized[3];
-	};
-	
-	User.prototype = {
-			id: 0,
-			name: "",
-			region: "",
-			battleNetProfiles: null
-	};
-})();
+User.prototype = {
+		id: 0,
+		name: "",
+		region: "",
+		battleNetProfiles: null
+};
--- website/public/js/gui/CharacterPlannerGui.js
+++ website/public/js/gui/CharacterPlannerGui.js
@@ -1,28 +1,24 @@
-var CharacterPlannerGui;
+/**
+ * @constructor
+ */
+function CharacterPlannerGui() {
 
-(function(){
-	/**
-	 * @constructor
-	 */
-	CharacterPlannerGui = function() {
-
-		this.characterInterface = new CharacterInterface();
-		
-		Dom.append(Dom.createAt("planner_parent", 'div', {'class': 'ci_p'}),this.characterInterface.node);
-		this.guiParent = Dom.createAt("planner_parent", "div", {});
-		
-		Dom.addClass("mtf_p", "cp_mm_p ix_center");
-	};
+	this.characterInterface = new CharacterInterface();
 	
-	CharacterPlannerGui.prototype = {
-		characterInterface: null,
-		guiParent: null,
-		/**
-		 * @param {Gui} gui
-		 */
-		setGui: function( gui ) {
-			Dom.set("mtf_p", gui.folder.menu);
-			Dom.set(this.guiParent, gui.node);
-		}
-	};
-})();
+	Dom.append(Dom.createAt("planner_parent", 'div', {'class': 'ci_p'}),this.characterInterface.node);
+	this.guiParent = Dom.createAt("planner_parent", "div", {});
+	
+	Dom.addClass("mtf_p", "cp_mm_p ix_center");
+}
+
+CharacterPlannerGui.prototype = {
+	characterInterface: null,
+	guiParent: null,
+	/**
+	 * @param {Gui} gui
+	 */
+	setGui: function( gui ) {
+		Dom.set("mtf_p", gui.folder.menu);
+		Dom.set(this.guiParent, gui.node);
+	}
+};
