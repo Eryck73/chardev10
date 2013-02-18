24bca2d3578aed56407fbdab41ce30526bb7234d
LeMartin <lemartin@chardev.org>
2013-02-18 07:47:13 +0100

    Added static tables to db export script

--- website/build/export_db.php
+++ website/build/export_db.php
@@ -6,9 +6,9 @@ $staticTables = implode(" ", array(
     "chardev_item_stats",
     "chardev_itemset_stats",
     "chardev_spellinfo",
-    "chardev_data_stats"
+    "chardev_base_stats_class_level",
 ));
 
 $path = __DIR__ . "/";
 exec("mysqldump -uroot chardev_mop > {$path}chardev_mop_b{$build}.sql && gzip {$path}chardev_mop_b{$build}.sql");
-exec("mysqldump -uroot chardev_mop_static > {$path}chardev_mop_static_b{$build}.sql && gzip {$path}chardev_mop_static_b{$build}.sql");
+exec("mysqldump -uroot chardev_mop_static $staticTables > {$path}chardev_mop_static_b{$build}.sql && gzip {$path}chardev_mop_static_b{$build}.sql");
